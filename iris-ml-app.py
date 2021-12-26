677f1e2e83e6ab8a5ecce8796a42eecef6ce2137
tumainilyimo <lyimotumaini@gmail.com>
2021-12-26 12:50:42 +0300

    Update iris-ml-app.py

--- iris-ml-app.py
+++ iris-ml-app.py
@@ -1,57 +1,55 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import seaborn as sns
-from sklearn import datasets
-from sklearn import manifold 
-
-
+#preg	plas	pres	skin	insulin	mass	pedi	age	result
+#import libraries
 import streamlit as st
 import pandas as pd
-from sklearn import datasets
+import numpy as np
 from sklearn.ensemble import RandomForestClassifier
-
+#app heading
 st.write("""
-# Simple Iris Flower Prediction App
-
-This app predicts the **Iris flower** type!
+# Wine Quality Prediction App
+This app predicts the ***Wine Quality*** type!
 """)
-
+#creating sidebar for user input features
 st.sidebar.header('User Input Parameters')
-
+  
 def user_input_features():
-    sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4)
-    sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
-    petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
-    petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2)
-    data = {'sepal_length': sepal_length,
-            'sepal_width': sepal_width,
-            'petal_length': petal_length,
-            'petal_width': petal_width}
-    features = pd.DataFrame(data, index=[0])
-    return features
-
+        preg = st.sidebar.slider('preg', 4.6, 15.9, 8.31)
+        plas = st.sidebar.slider('plas', 0.12,1.58 , 0.52)
+        pres = st.sidebar.slider('pres', 0.0,1.0 , 0.5)
+        skin = st.sidebar.slider('skin', 0.01,0.6 , 0.08)
+        insulin=st.sidebar.slider('insulin', 6.0,289.0 , 46.0)
+        mass=st.sidebar.slider('mass', 8.4,14.9, 10.4)
+        pedi=st.sidebar.slider('pedi', 0.33,2.0,0.65 )
+        age=st.sidebar.slider('age', 0.33,2.0,0.65 )
+        data = {'preg': preg,
+                'plas': plas,
+                'pres': pres,
+                'skin': skin,
+              'insulin':insulin,
+              'mass':mass,
+                'pedi':pedi
+               }
+        features = pd.DataFrame(data, index=[0])
+        return features
 df = user_input_features()
 
 st.subheader('User Input parameters')
 st.write(df)
+#reading csv file
+data=pd.read_csv("./fulltrainset.csv")
+X =np.array(data[['preg', 'plas' , 'pres' , 'skin' , 'insulin' , 'mass' , 'pedi']])
+Y = np.array(data['result'])
+#random forest model
+rfc= RandomForestClassifier()
+rfc.fit(X, Y)
+st.subheader('Wine quality labels and their corresponding index number')
+st.write(pd.DataFrame({
+   'wine quality': [3, 4, 5, 6, 7, 8, 9 ]}))
 
-iris = datasets.load_iris()
-X = iris.data
-Y = iris.target
-
-clf = RandomForestClassifier()
-clf.fit(X, Y)
-
-prediction = clf.predict(df)
-prediction_proba = clf.predict_proba(df)
-
-st.subheader('Class labels and their corresponding index number')
-st.write(iris.target_names)
-
+prediction = rfc.predict(df)
+prediction_proba = rfc.predict_proba(df)
 st.subheader('Prediction')
-st.write(iris.target_names[prediction])
-#st.write(prediction)
+st.write(prediction)
 
 st.subheader('Prediction Probability')
 st.write(prediction_proba)
