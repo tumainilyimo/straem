568bce1fed4fe919a005e6a785abd384d41bac14
tumainilyimo <lyimotumaini@gmail.com>
2021-12-26 13:01:22 +0300

    Update iris-ml-app.py

--- iris-ml-app.py
+++ iris-ml-app.py
@@ -1,6 +1,6 @@
 #preg	plas	pres	skin	insulin	mass	pedi	age	result
 #import libraries
-sudo install scikit-learn
+conda install scikit-learn
 import streamlit as st
 import pandas as pd
 import numpy as np
